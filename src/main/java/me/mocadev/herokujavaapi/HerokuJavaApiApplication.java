826204c16ec2057e60a15edb674f1fd36b9f1e56
tony <chcjswo@gmail.com>
2022-10-02 01:14:21 +0900

    Initial Project

---
+++ src/main/java/me/mocadev/herokujavaapi/HerokuJavaApiApplication.java
@@ -0,0 +1,13 @@
+package me.mocadev.herokujavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuJavaApiApplication.class, args);
+	}
+
+}
