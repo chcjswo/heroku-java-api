eff23bb5c74951cc7a1fbdeeb6dda8b22f71b030
tony <chcjswo@gmail.com>
2023-02-19 01:21:29 +0900

    #2-mongodb 설정
    * document 설정
    * repository 설정

---
+++ src/main/java/me/mocadev/controller/musicsheet/MusicSheetController.java
@@ -0,0 +1,22 @@
+package me.mocadev.controller.musicsheet;
+
+import javax.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 콘티 공유 Controller
+ *
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2023-02-18
+ **/
+@RestController
+public class MusicSheetController {
+	@PostConstruct
+	public void init() {
+
+	}
+
+}
---
+++ src/main/java/me/mocadev/document/musicsheet/Music.java
@@ -0,0 +1,51 @@
+package me.mocadev.document.musicsheet;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+/**
+ * 콘티 공유 Document
+ *
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2023-02-19
+ **/
+@Data
+@Document(collection = "musics")
+public class Music {
+
+	@Id
+	private String _id;
+	@Field("room_name")
+	private String roomName;
+	@Field("room_pass")
+	private String roomPass;
+	@Field("music_sheet")
+	private List<MusicSheet> musicSheets;
+	@Field("video_url")
+	private String videoUrl;
+	private String memo;
+	@Field("random_string")
+	private String randomString;
+	@Field("reg_date")
+	private LocalDate regDate;
+
+	@Builder
+	public Music(String roomName, String roomPass, List<MusicSheet> musicSheets, String videoUrl,
+				 String memo, String randomString) {
+		this.roomName = roomName;
+		this.roomPass = roomPass;
+		this.musicSheets = musicSheets;
+		this.videoUrl = videoUrl;
+		this.memo = memo;
+		this.randomString = randomString;
+		this.regDate = LocalDate.now();
+	}
+}
