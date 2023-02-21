a76edf79483431487df9a087204e2d0dc00f2d8d
tony <chcjswo@gmail.com>
2023-02-21 23:27:15 +0900

    #4 - 악보 Document _id ObjectId 타입으로 변경

--- src/main/java/me/mocadev/herokujavaapi/document/musicsheet/Music.java
+++ src/main/java/me/mocadev/herokujavaapi/document/musicsheet/Music.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -24,7 +25,7 @@ public class Music {
 
 	@Id
 	@Field("_id")
-	private String id;
+	private ObjectId id;
 	@Field("room_name")
 	@Indexed(unique = true)
 	private String roomName;
@@ -41,8 +42,12 @@ public class Music {
 	private LocalDateTime regDate;
 
 	@Builder
-	public Music(String roomName, String roomPass, List<MusicSheet> musicSheets, String videoUrl,
-				 String memo, String randomString) {
+	public Music(String roomName,
+				 String roomPass,
+				 List<MusicSheet> musicSheets,
+				 String videoUrl,
+				 String memo,
+				 String randomString) {
 		this.roomName = roomName;
 		this.roomPass = roomPass;
 		this.musicSheets = musicSheets;
