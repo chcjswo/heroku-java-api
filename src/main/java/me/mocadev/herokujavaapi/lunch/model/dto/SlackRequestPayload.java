65ded9653693e00470925a3fde7ecd6634349d73
tony <chcjswo@gmail.com>
2023-05-20 17:03:49 +0900

    decision payload 수정

--- src/main/java/me/mocadev/herokujavaapi/lunch/model/dto/SlackRequestPayload.java
+++ src/main/java/me/mocadev/herokujavaapi/lunch/model/dto/SlackRequestPayload.java
@@ -2,8 +2,6 @@ package me.mocadev.herokujavaapi.lunch.model.dto;
 
 import lombok.Data;
 
-import java.util.List;
-
 /**
  * @author chcjswo
  * @version 1.0.0
@@ -13,36 +11,36 @@ import java.util.List;
  **/
 @Data
 public class SlackRequestPayload {
-	private User user;
-	private List<Actions> actions;
-	private Payload payload;
-
-	@Data
-	public static class User {
-		private String id;
-		private String username;
-		private String team_id;
-	}
-
-	@Data
-	public static class Actions {
-		private String action_id;
-		private String block_id;
-		private Text text;
-		private String value;
-		private String action_ts;
-	}
-
-	@Data
-	public static class Text {
-		private String type;
-		private boolean emoji;
-	}
-
-	@Data
-	public static class Payload {
-		private User user;
-		private List<Actions> actions;
-	}
+//	private User user;
+//	private List<Actions> actions;
+//	private Payload payload;
+//
+//	@Data
+//	public static class User {
+//		private String id;
+//		private String username;
+//		private String team_id;
+//	}
+//
+//	@Data
+//	public static class Actions {
+//		private String action_id;
+//		private String block_id;
+//		private Text text;
+//		private String value;
+//		private String action_ts;
+//	}
+//
+//	@Data
+//	public static class Text {
+//		private String type;
+//		private boolean emoji;
+//	}
+//
+//	@Data
+//	public static class Payload {
+//		private User user;
+//		private List<Actions> actions;
+//	}
 }
 
